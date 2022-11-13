b59e8490528802f3d920618fdaa6309a53dcd8b4
이태헌 <xogjs0714@naver.com>
2022-11-14 01:14:14 +0900

    11주차 과제(미완)

--- 10주차/MuSoenMath.h
+++ 10주차/MuSoenMath.h
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cmath>
 
+#define PI 3.1415926 //sin cos값을 위한 파이값
 using namespace std;
+
 class Mat3 { 
 public:
     float Mat[3][3]; // 2차원행렬
@@ -13,63 +15,434 @@ public:
             }
         }
     }
-    void Reset() { // 행렬 초기화 함수
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                Mat[i][j] = 0;
+    
+    Mat3(float _00, float _01, float _02,
+        float _10, float _11, float _12,
+        float _20, float _21, float _22)
+    {
+        Mat[0][0] = _00;
+        Mat[0][1] = _01;
+        Mat[0][2] = _02;
+        Mat[1][0] = _10;
+        Mat[1][1] = _11;
+        Mat[1][2] = _12;
+        Mat[2][0] = _20;
+        Mat[2][1] = _21;
+        Mat[2][2] = _22;
+    }//행렬의 값을 입력받는 생성자
+
+    Mat3 identity_matrix()
+    {
+        return Mat3(
+            1, 0, 0,
+            0, 1, 0,
+            0, 0, 1);
+    }//단위행렬를 반환하는 함수
+
+    Mat3 transpose_matrix()
+    {
+        Mat3 result;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                result.Mat[i][j] = Mat[j][i];
             }
         }
-        Mat[2][2] = 1; //2차원 좌표에서 z축은 존재하지 않음으로 1로 초기화
-    }
 
-    void Translate(float x, float y) 
-    { 
-        Reset();
-        Mat[0][0] = 1;
-        Mat[1][1] = 1;
-        Mat[2][0] = x; 
-        Mat[2][1] = y; 
-        // 이동 변환 행렬 공식에 맞게 설정
-    }
+        return result;
+    }//전치행렬로 변환하는 함수
 
-    void Rotate(float r) 
+    Mat3 multiply_matrix()
     {
-        Reset();
-        double Rotate = r * (3.14 / 180); // 라디안 설정
-        Mat[0][0] = cos(Rotate);
-        Mat[0][1] = (-sin(Rotate));
-        Mat[1][0] = sin(Rotate);
-        Mat[1][1] = cos(Rotate); 
-        // 회전 변환 행렬 공식에 맞게 설정
+       
     }
 
-    void Scale(float s) 
-    { 
-        Reset();
-        Mat[0][0] = s;
-        Mat[1][1] = s; 
-        // 크기 변환 행렬 공식에 맞게 설정
-    }
+    Mat3 multiply_transpose_matrix()
+    {
 
+    }
 };
 
 class Vec3 { 
 public:
-    float Position[2]; // 2차원좌표+동차좌표
+    float x, y,z;
 
-    Vec3(float x, float y) { // 생성자
-        Position[0] = x;
-        Position[1] = y;
+    Vec3()
+    {
+        x = 0;
+        y = 0;
+        z = 1;
+    }//값을 입력받지 않았을때의 원점
+
+    Vec3(float X, float Y, float Z)
+    {
+        x = X;
+        y = Y;
+        z = Z;
+    }//값을 입력받아서 원점을 설정
+
+    Vec3 multiply_vector()
+    {
+
     }
 
-    Vec3 operator *(Mat3& p) // 연산자 오버로딩
+    Vec3 multiply_transpose_vector()
     {
-        float x = Position[0]; // 3*1행렬의 x설정
-        float y = Position[1]; // 3*1행렬의 y설정
-        float z = 1; // 3*1행렬의 z설정
-        float a = (x * p.Mat[0][0]) + (y * p.Mat[1][0]) + (z * p.Mat[2][0]); // 행렬곱 x좌표 결과값 저장
-        float b = (x * p.Mat[0][1]) + (y * p.Mat[1][1]) + (z * p.Mat[2][1]); // 행렬곱 y좌표 결과값 저장
-        // Z좌표는 2차원에선 필요없으므로 생략
-        return Vec3(a, b); // 저장된 x,y좌표 리턴
+
     }
 };
+
+const Vec3 operator * (const Mat3& ref, const Vec3& vec) //vec3와 3*3행렬의 곱셈을 구현
+{
+    Vec3 result;
+    Mat3 mat = ref;
+    mat = mat.transpose_matrix(); //전치행렬로 변환
+
+    float x = vec.x; // 3*1행렬의 x설정
+    float y = vec.y; // 3*1행렬의 y설정
+    float z = vec.z;
+    result.x = (x * mat.Mat[0][0]) + (y * mat.Mat[0][1]) + (z * mat.Mat[0][2]);
+    result.y = (x * mat.Mat[1][0]) + (y * mat.Mat[1][1]) + (z * mat.Mat[1][2]);
+    result.z = (x * mat.Mat[2][0]) + (y * mat.Mat[2][1]) + (z * mat.Mat[2][2]);
+    return result;
+}
+
+const Vec3 operator * (const Vec3& vec, const Mat3& ref) //vec3와 3*3행렬의 곱셈을 구현
+{
+    Vec3 result;
+    Mat3 mat = ref;
+    mat = mat.transpose_matrix(); //전치행렬로 변환
+
+    float x = vec.x; // 3*1행렬의 x설정
+    float y = vec.y; // 3*1행렬의 y설정
+    float z = vec.z; // 3*1행렬의 z설정
+    result.x = (x * mat.Mat[0][0]) + (y * mat.Mat[0][1]) + (z * mat.Mat[0][2]); 
+    result.y = (x * mat.Mat[1][0]) + (y * mat.Mat[1][1]) + (z * mat.Mat[1][2]);
+    result.z = (x * mat.Mat[2][0]) + (y * mat.Mat[2][1]) + (z * mat.Mat[2][2]);
+    return result;
+}
+
+const Mat3 operator * (const Mat3& mat, const Mat3& ref) //3*3행렬 끼리의 곱셈을 구현
+{
+    Mat3 result;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++)
+        {
+            result.Mat[i][j] = mat.Mat[i][j] * ref.Mat[i][j];
+        }
+    }
+
+    return result;
+}
+
+const Vec3 operator * (Vec3 vec, Vec3 ref) //3*1벡터 끼리의 곱셈을 구현
+{
+    Vec3 result;
+    result.x = vec.x * ref.x;
+    result.y = vec.y * ref.y;
+    result.z = vec.z * ref.z;
+    return result;
+}
+
+const Mat3 operator / (const Mat3& mat, const Mat3& ref) //3*3행렬 끼리의 나눗셈을 구현
+{
+    Mat3 result;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++)
+        {
+            result.Mat[i][j] = mat.Mat[i][j] / ref.Mat[i][j];
+        }
+    }
+
+    return result;
+}
+
+const Vec3 operator / (Vec3 vec, Vec3 ref) //3*1벡터 끼리의 나눗셈을 구현
+{
+    Vec3 result;
+    result.x = vec.x / ref.x;
+    result.y = vec.y / ref.y;
+    result.z = vec.z / ref.z;
+    return result;
+}
+
+const Mat3 operator + (const Mat3& mat, const Mat3& ref) //3*3행렬 끼리의 덧셈을 구현
+{
+    Mat3 result;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) 
+        {
+            result.Mat[i][j] = mat.Mat[i][j] + ref.Mat[i][j];
+        }
+    }
+
+    return result;
+}
+
+const Vec3 operator + (Vec3 vec, Vec3 ref) //3*1벡터 끼리의 덧셈을 구현
+{
+    Vec3 result;
+    result.x = vec.x + ref.x;
+    result.y = vec.y + ref.y;
+    result.z = vec.z + ref.z;
+    return result;
+}
+
+const Mat3 operator - (const Mat3& mat, const Mat3& ref) //3*3행렬 끼리의 뺄셈을 구현
+{
+    Mat3 result;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++)
+        {
+            result.Mat[i][j] = mat.Mat[i][j] - ref.Mat[i][j];
+        }
+    }
+
+    return result;
+}
+
+const Vec3 operator - (Vec3 vec, Vec3 ref) //3*1벡터 끼리의 뺄셈을 구현
+{
+    Vec3 result;
+    result.x = vec.x - ref.x;
+    result.y = vec.y - ref.y;
+    result.z = vec.z - ref.z;
+    return result;
+}
+
+class Mat4 {
+public:
+    float Mat[4][4]; // 3차원행렬
+
+    Mat4() { //  행렬 생성자
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                Mat[i][j] = 0; // 멤버 변수 초기화
+            }
+        }
+    }
+
+    Mat4(float _00, float _01, float _02, float _03,
+        float _10, float _11, float _12, float _13,
+        float _20, float _21, float _22, float _23,
+        float _30, float _31, float _32, float _33)
+    {
+        Mat[0][0] = _00;
+        Mat[0][1] = _01;
+        Mat[0][2] = _02;
+        Mat[0][3] = _03;
+        Mat[1][0] = _10;
+        Mat[1][1] = _11;
+        Mat[1][2] = _12;
+        Mat[1][3] = _13;
+        Mat[2][0] = _20;
+        Mat[2][1] = _21;
+        Mat[2][2] = _22;
+        Mat[2][3] = _23;
+        Mat[3][0] = _30;
+        Mat[3][1] = _31;
+        Mat[3][2] = _32;
+        Mat[3][3] = _33;
+    }//행렬의 값을 입력받는 생성자
+
+    Mat4 identity_matrix()
+    {
+        return Mat4(
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1);
+    }//단위행렬를 반환하는 함수
+
+    Mat4 transpose_matrix()
+    {
+        Mat4 result;
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                result.Mat[i][j] = Mat[j][i];
+            }
+        }
+
+        return result;
+    }//전치행렬로 변환하는 함수
+
+    Mat4 multiply_matrix()
+    {
+
+    }
+
+    Mat4 multiply_transpose_matrix()
+    {
+
+    }
+};
+
+class Vec4 {
+public:
+    float x, y, z,w;
+
+    Vec4()
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+        w = 1;
+    }//값을 입력받지 않았을때의 원점
+
+    Vec4(float X, float Y, float Z, float W)
+    {
+        x = X;
+        y = Y;
+        z = Z;
+        w = W;
+    }//값을 입력받아서 원점을 설정
+
+    Vec4 multiply_vector()
+    {
+
+    }
+
+    Vec4 multiply_transpose_vector()
+    {
+
+    }
+};
+
+const Vec4 operator * ( Mat4 ref,  Vec4 vec) //vec4와 4*4행렬의 곱셈을 구현
+{
+    Vec4 result;
+    Mat4 mat = ref;
+    mat = mat.transpose_matrix(); //전치행렬로 변환
+
+    float x = vec.x; // 4*1행렬의 x설정
+    float y = vec.y; // 4*1행렬의 y설정
+    float z = vec.z; // 4*1행렬의 z설정
+    float w = vec.w; // 4*1행렬의 w설정
+    result.x = (x * mat.Mat[0][0]) + (y * mat.Mat[0][1]) + (z * mat.Mat[0][2]) + (w * mat.Mat[0][3]);
+    result.y = (x * mat.Mat[1][0]) + (y * mat.Mat[1][1]) + (z * mat.Mat[1][2]) + (w * mat.Mat[1][3]);
+    result.z = (x * mat.Mat[2][0]) + (y * mat.Mat[2][1]) + (z * mat.Mat[2][2]) + (w * mat.Mat[2][3]);
+    result.w = (x * mat.Mat[3][0]) + (y * mat.Mat[3][1]) + (z * mat.Mat[3][2]) + (w * mat.Mat[3][3]);
+    return result;
+}
+
+const Vec4 operator * ( Vec4 vec,  Mat4 ref) //vec4와 4*4행렬의 곱셈을 구현
+{
+    Vec4 result;
+    Mat4 mat = ref;
+    mat = mat.transpose_matrix(); //전치행렬로 변환
+
+    float x = vec.x; // 4*1행렬의 x설정
+    float y = vec.y; // 4*1행렬의 y설정
+    float z = vec.z; // 4*1행렬의 z설정
+    float w = vec.w; // 4*1행렬의 w설정
+    result.x = (x * mat.Mat[0][0]) + (y * mat.Mat[0][1]) + (z * mat.Mat[0][2]) + (w * mat.Mat[0][3]);
+    result.y = (x * mat.Mat[1][0]) + (y * mat.Mat[1][1]) + (z * mat.Mat[1][2]) + (w * mat.Mat[1][3]);
+    result.z = (x * mat.Mat[2][0]) + (y * mat.Mat[2][1]) + (z * mat.Mat[2][2]) + (w * mat.Mat[2][3]);
+    result.w = (x * mat.Mat[3][0]) + (y * mat.Mat[3][1]) + (z * mat.Mat[3][2]) + (w * mat.Mat[3][3]);
+    return result;
+}
+
+const Mat4 operator * ( Mat4 mat, Mat4 ref) //4*4행렬 끼리의 곱셈을 구현
+{
+    Mat4 result;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+        {
+            result.Mat[i][j] = mat.Mat[i][j] * ref.Mat[i][j];
+        }
+    }
+
+    return result;
+}
+
+const Vec4 operator * (Vec4 vec, Vec4 ref) //4*1벡터 끼리의 곱셈을 구현
+{
+    Vec4 result;
+    result.x = vec.x * ref.x;
+    result.y = vec.y * ref.y;
+    result.z = vec.z * ref.z;
+    result.w = vec.w * ref.w;
+    return result;
+}
+
+const Mat4 operator / (Mat4 mat, Mat4 ref) //4*4행렬 끼리의 나눗셈을 구현
+{
+    Mat4 result;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+        {
+            result.Mat[i][j] = mat.Mat[i][j] / ref.Mat[i][j];
+        }
+    }
+
+    return result;
+}
+
+const Vec4 operator / (Vec4 vec, Vec4 ref) //4*1벡터 끼리의 나눗셈을 구현
+{
+    Vec4 result;
+    result.x = vec.x / ref.x;
+    result.y = vec.y / ref.y;
+    result.z = vec.z / ref.z;
+    result.w = vec.w / ref.w;
+    return result;
+}
+
+const Mat4 operator + (Mat4 mat, Mat4 ref) //4*4행렬 끼리의 덧셈을 구현
+{
+    Mat4 result;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+        {
+            result.Mat[i][j] = mat.Mat[i][j] + ref.Mat[i][j];
+        }
+    }
+
+    return result;
+}
+
+const Vec4 operator + (Vec4 vec, Vec4 ref) //4*1벡터 끼리의덧셈을 구현
+{
+    Vec4 result;
+    result.x = vec.x + ref.x;
+    result.y = vec.y + ref.y;
+    result.z = vec.z + ref.z;
+    result.w = vec.w + ref.w;
+    return result;
+}
+
+const Mat4 operator - (Mat4 mat, Mat4 ref) //4*4행렬 끼리의 뺄셈을 구현
+{
+    Mat4 result;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+        {
+            result.Mat[i][j] = mat.Mat[i][j] - ref.Mat[i][j];
+        }
+    }
+
+    return result;
+}
+
+const Vec4 operator - (Vec4 vec, Vec4 ref) //4*1벡터 끼리의 뺄셈을 구현
+{
+    Vec4 result;
+    result.x = vec.x - ref.x;
+    result.y = vec.y - ref.y;
+    result.z = vec.z - ref.z;
+    result.w = vec.w - ref.w;
+    return result;
+}
--- 10주차/소스.cpp
+++ 10주차/소스.cpp
@@ -1,20 +1,66 @@
 #include "MuSoenMath.h"
 
-void main() {
+void main()
+{
+	Vec4 v;
 
-	Vec3 Vector(0, 0); //원점을 0,0으로 기본백터 설정
-	Mat3 Matrix;
+	Mat4 Translate = Mat4(
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		3, 5, 1, 1); //이동행렬 입력
 
-	Matrix.Translate(3, 5); // 3,5로 이동
-	Vector = Vector * Matrix; // 연산자 오버로딩으로 백터와 행렬 계산
-	Matrix.Rotate(30); // 30도 회전
-	Vector = Vector * Matrix;// 연산자 오버로딩으로 백터와 행렬 계산
-	Matrix.Scale(2); // 크기를 2배로 확대
-	Vector = Vector * Matrix;// 연산자 오버로딩으로 백터와 행렬 계산
+	Mat4 Rotate = Mat4(
+		cos(30 * PI / 180), -sin(30 * PI / 180), 0, 0,
+		sin(30 * PI / 180), cos(30 * PI / 180), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1);//회전행렬 입력
+
+	Mat4 Scale = Mat4(
+		2, 0, 0, 0,
+		0, 2, 0, 0,
+		0, 0, 2, 0,
+		0, 0, 0, 1);//크기 행렬 입력
+
+	Vec4 p = v * Translate * Rotate * Scale;
+
+	Mat3 a = Mat3(
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9
+	);
+
+	Mat3 b = Mat3(
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9
+	);
+
+	Mat3 c = a * b;
+
+	Vec4 w = Vec4(1, 2, 3, 4);
+	Vec4 e = Vec4(1, 2, 3, 4);
+
+	Vec4 r = w + e;
 
 	cout << fixed;
 	cout.precision(0); // 소수점 제거
 
-	cout << "원점이 0,0일때 3,5이동 30도회전 2배확대했을때의 좌표값은: "<<  endl;
-	cout << Vector.Position[0] << "," << Vector.Position[1] << "입니다." << endl;
+	cout << "원점이 0,0,0일때 3,5이동 30도회전 2배확대했을때의 좌표값은: " << endl;
+	cout << p.x << "," << p.y << "," << p.z << "입니다." << endl;
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << c.Mat[i][j];
+			if (j < 2) cout << " ";
+		}
+		cout << endl;
+	}
+
+	cout << r.x << endl;
+	cout << r.y << endl;
+	cout << r.z << endl;
+	cout << r.w << endl;
 }
